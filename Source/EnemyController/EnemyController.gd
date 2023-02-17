47c171cb537913c899ba28757f671ced8a7735c7
DeltaPi314 <kevin12.nolle@gmx.de>
2023-02-17 13:51:05 +0100

    changed enemy spawner to use nodes instead of scenes added functions for maximum enemy limitations

--- Source/EnemyController/EnemyController.gd
+++ Source/EnemyController/EnemyController.gd
@@ -1,6 +1,7 @@
 extends Node2D
-export var timer=1
-var radius
+export(float) var timer=1.0
+export(int) var maximum=300
+var radius #radius outside of vision of player
 var time_elapsed=0
 
 # Called when the node enters the scene tree for the first time.
@@ -12,7 +13,11 @@ func _ready():
 func _process(delta):
 	radius=get_viewport_rect().end.length()+50
 	
-	
+	print(get_enemy_count())
+	var enemy_overflow=get_enemy_count()-maximum
+	if(enemy_overflow>0):
+		delete(enemy_overflow)
+		
 	timer-=delta
 	time_elapsed+=delta
 	if(timer<0):
@@ -22,13 +27,19 @@ func _process(delta):
 				spawn_wave(wave)
 			
 func spawn_wave(wave):
-	var exponential_count = wave.base_count * pow( wave.base, time_elapsed )
-	var polynomial_count  = wave.base_count * pow( time_elapsed,wave.exponent )
+	var exponential_count = wave.base_count * pow(wave.base, time_elapsed)
+	var polynomial_count  = wave.base_count * pow(time_elapsed,wave.exponent)
 	var count =  (exponential_count+polynomial_count) * wave.linear_factor
 	count=floor(count)
-	print(count)
+	
+	#delete enemies if not enough space
+	var enemy_space=maximum-get_enemy_count()
+	if(enemy_space<count):
+		count=delete(count-enemy_space)+enemy_space
+	
 	for j in count:
-		spawn_enemy(wave,wave.scene_path)
+		var scene=wave.packed_scene
+		spawn_enemy(wave,scene)
 	pass
 	
 func spawn_enemy(wave,scene):
@@ -38,3 +49,23 @@ func spawn_enemy(wave,scene):
 	enemy.position=pos
 	wave.add_child(enemy)
 	pass
+	
+func get_enemy_count():
+	var count=0
+	for wave in get_children():
+		count+=wave.get_child_count()
+	return count
+
+func delete(count): #delete enemies, return amount of deleted enemies
+	var deleted=0
+	for wave in get_children():
+		if(wave.delete_when_full):
+			for enemy in wave.get_children():
+				if(enemy.position.distance_to(Player.position) > radius):
+					enemy.queue_free()
+					count-=1
+					deleted+=1
+					if(count<=0):
+						return deleted
+	return deleted
+	
--- Source/EnemyController/SpawnWave.gd
+++ Source/EnemyController/SpawnWave.gd
@@ -5,10 +5,19 @@ export(float) var linear_factor = 1
 export(float) var exponent = 1
 export(float) var base = 0
 export(float) var base_count = 1
-export(PackedScene) var scene_path
+export(NodePath) var entity
+export(bool) var delete_when_full = true
+var packed_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if(get_node(entity) == null):
+		print("Warning: Spawnwave without assigned entity, assigning child")
+		entity = get_children()[0].get_path()
+	var scene = PackedScene.new()
+	scene.pack(get_node(entity))
+	packed_scene=scene
+	get_node(entity).queue_free()
 	pass # Replace with function body.
 
 
