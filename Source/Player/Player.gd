6bc4a97c3eb589838f380a73d2ae212822c1d4b4
DeltaPi314 <kevin12.nolle@gmx.de>
2023-02-20 16:33:15 +0100

    added xp drops, collection and level up method

--- Source/Player/Player.gd
+++ Source/Player/Player.gd
@@ -5,20 +5,22 @@ extends KinematicBody2D
 export var health = 20
 export var speed = 1000
 export var invincibleSeconds=0
+var xp=0
+var xp_needed=1
+var level=0
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$XPCollector/CollisionShape2D.shape.radius=300 #change collection radius
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if(xp>=xp_needed):
+		on_level_up()
 	pass
-
-func playerHit(enemy):
-	if(invincibleSeconds==0):
-		health -= enemy.collision_damage
-		invincibleSeconds=0.5
-		print(health)
 
 func _physics_process(delta):
 	invincibleSeconds=max(invincibleSeconds-delta,0)
@@ -48,3 +50,18 @@ func _input(event):#turn to mouse
 		var vec=event.position/get_viewport_rect().size-Vector2(0.5,0.5)
 		rotation=vec.angle()-PI/2
 
+func _on_XPCollector_area_entered(area):
+	if(area.is_in_group("collectable")):
+		area.caught=true
+
+func playerHit(enemy):
+	if(invincibleSeconds==0):
+		health -= enemy.collision_damage
+		invincibleSeconds=0.5
+		print(health)
+
+func on_level_up():
+	xp-=xp_needed
+	level+=1
+	xp_needed = level*5
+	print("level up")
