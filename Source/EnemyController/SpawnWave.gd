d7976f31b8d61ddb3b96b13ab05497291b051344
Masterflitzer <60170948+masterflitzer@users.noreply.github.com>
2023-02-21 22:35:33 +0100

    GitHub linguist gdscript detection (#10)
    * fix github linguist for this godot project
    * fix multiple consecutive line breaks

--- Source/EnemyController/SpawnWave.gd
+++ Source/EnemyController/SpawnWave.gd
@@ -20,7 +20,6 @@ func _ready():
 	get_node(entity).queue_free()
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
