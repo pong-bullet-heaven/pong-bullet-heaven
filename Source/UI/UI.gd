d2a6b65ccb9b8721e9ec1883351de72e8f00d42b
masterflitzer <60170948+masterflitzer@users.noreply.github.com>
2023-03-17 17:39:35 +0100

    update toggle fullscreen code

--- Source/UI/UI.gd
+++ Source/UI/UI.gd
@@ -3,17 +3,18 @@ extends CanvasLayer
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if (Input.is_action_just_pressed("toggle_fullscreen")):
 		toggle_fullscreen()
 
 func toggle_fullscreen():
 	if (OS.window_fullscreen):
-		var screen = OS.get_screen_size()
-		var base = Vector2(1024, 576)
-		var div = screen / base
-		OS.window_size = floor(min(div[0], div[1])) * base
+		var screen_size = OS.get_screen_size()
+		var base = Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
+		var div = screen_size / base
+		var window_size = base * floor(min(div[0], div[1]))
+		OS.window_size = window_size
 		OS.window_fullscreen = false
+		OS.set_window_position(0.5 * screen_size - 0.5 * window_size)
 	else:
 		OS.window_fullscreen = true
