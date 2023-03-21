dd9aff5e6bad52a424531566fadf147e0f0c76cf
masterflitzer <60170948+masterflitzer@users.noreply.github.com>
2023-03-21 15:44:03 +0100

    implement toggle sound with F10

--- Source/UI/UI.gd
+++ Source/UI/UI.gd
@@ -1,11 +1,17 @@
 extends CanvasLayer
 
+
 func _ready():
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
 
 func _process(_delta):
 	if (Input.is_action_just_pressed("toggle_fullscreen")):
 		toggle_fullscreen()
+
+	if (Input.is_action_just_pressed("toggle_sound")):
+		toggle_sound()
+
 
 func toggle_fullscreen():
 	if (OS.window_fullscreen):
@@ -18,3 +24,9 @@ func toggle_fullscreen():
 		OS.set_window_position(0.5 * screen_size - 0.5 * window_size)
 	else:
 		OS.window_fullscreen = true
+
+
+func toggle_sound():
+	var idx = AudioServer.get_bus_index("Master")
+	var is_mute = AudioServer.is_bus_mute(idx)
+	AudioServer.set_bus_mute(idx, !is_mute)
