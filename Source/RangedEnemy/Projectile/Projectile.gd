23cbbb6aa1b0a3052a86affeec92a4ac3a84d31d
masterflitzer <60170948+masterflitzer@users.noreply.github.com>
2023-03-25 01:11:27 +0100

    code style

--- Source/RangedEnemy/Projectile/Projectile.gd
+++ Source/RangedEnemy/Projectile/Projectile.gd
@@ -4,28 +4,28 @@ export var projectile_damage = 1
 export(int) var speed: int = 300
 var direction: Vector2 = Vector2.LEFT
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	translate(direction*speed*delta)
 	pass
+
+
+func _physics_process(delta):
+	translate(direction * speed * delta)
+
+
 func destroy():
 	queue_free()
-
 
 
 #func _on_Projectile_area_entered(area):
 #	destroy()
 
 
-func _on_Projectile_body_entered(Player):
-	Player.health -= projectile_damage
-	print(Player.health)
-	destroy()
+func _on_Projectile_body_entered(body):
+	if body == Player:
+		body.health -= projectile_damage
+		print(body.health)
+		destroy()
 
 
 func _on_VisibilityNotifier2D_screen_exited():
--- Source/RangedEnemy/RangedEnemy.gd
+++ Source/RangedEnemy/RangedEnemy.gd
@@ -1,82 +1,93 @@
 extends KinematicBody2D
 
-export var collision_damage=1
-export var speed =50
+export var collision_damage = 1
+export var speed = 50
 export var health = 3
-export var backwardsspeed= -70
+export var backwardsspeed = -70
 export var Projectile = preload("res://Source/RangedEnemy/Projectile/Projectile.tscn")
-var damageAnimSeconds=0
+var damage_anim_seconds = 0
 var attacktimercount = 1
-var timero = 0 #timer for moving back in a sprint
+var timero = 0  # timer for moving back in a sprint
 var move_back = false
+
+
 func _ready():
 	pass
 
-func _process(delta):	# no damage animation yet available
-	damageAnimSeconds=max(damageAnimSeconds-delta,0)
-#	if(damageAnimSeconds>0):
+
+func _process(delta):  # no damage animation yet available
+	damage_anim_seconds = max(damage_anim_seconds - delta, 0)
+
+
+#	if(damage_anim_seconds > 0):
 #		$AnimatedSprite.play("damage")
 #	else:
 #		$AnimatedSprite.play("default")
-	pass
+
 
 func move_forward():
-	if (timero == 0):
-		var v=(Player.position-position).normalized() * speed
-		rotation=v.angle()-PI/2
+	if timero == 0:
+		var v = (Player.position - position).normalized() * speed
+		rotation = v.angle() - PI / 2
 		move_and_slide(v)
 	else:
 		move_backward()
 
+
 func move_backward():
-	if (timero != 0): #timer that counts down the backwards sprint
+	if timero != 0:  # timer that counts down the backwards sprint
 		timero -= 1
-		var c=(Player.position-position).normalized() * backwardsspeed
-		rotation=c.angle()-PI/2
+		var c = (Player.position - position).normalized() * backwardsspeed
+		rotation = c.angle() - PI / 2
 		move_and_slide(c)
-	else:timero=50
-	
-func _physics_process(delta):
-	if(move_back == false):
+	else:
+		timero = 50
+
+
+func _physics_process(_delta):
+	if move_back == false:
 		move_forward()
-		attacktimercount = AttackTimer(attacktimercount)
-		
-	elif(move_back == true):
+		attacktimercount = attack_timer(attacktimercount)
+	elif move_back == true:
 		move_backward()
-	
-func AttackTimer(attacktimercount):
-	attacktimercount = attacktimercount -1 
-	if(attacktimercount==0):
+
+
+func attack_timer(attacktimercount):
+	attacktimercount = attacktimercount - 1
+	if attacktimercount == 0:
 		shoot()
-		attacktimercount = 800 # this is the attackspeed
+		attacktimercount = 800  # this is the attack speed
 	return attacktimercount
-	
-	
-func shoot(): #shoot the projectile
+
+
+func shoot():  # shoot the projectile
 	var projectile = Projectile.instance()
 	get_tree().get_root().add_child(projectile)
 	projectile.global_position = global_position
 	var dir = (Player.global_position - global_position).normalized()
 	projectile.global_rotation = dir.angle() + PI / 2.0
 	projectile.direction = dir
-	
+
+
 func damage(amount):
-	damageAnimSeconds=0.5
-	health-=amount
-	if(health<=0):
+	damage_anim_seconds = 0.5
+	health -= amount
+	if health <= 0:
 		die()
+
 
 func die():
 	var scene = load("res://Source/XPDrops/XP.tscn")
 	var instance = scene.instance()
-	instance.position=position
-	get_node("/root").add_child(instance)
-
+	instance.position = position
+	get_node("/root").call_deferred("add_child", instance)
 	queue_free()
-	
+
+
 func _on_Range_body_entered(body):
 	if body.is_in_group("Player"):
 		move_back = true
 
-func _on_Range_body_exited(body):
+
+func _on_Range_body_exited(_body):
 	move_back = false
